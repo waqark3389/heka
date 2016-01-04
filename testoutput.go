d898cf6aaf71e8ccb12e7e0b83a69b0d9b24bd2a
waqark3389 <waqark3389@gmail.com>
2016-01-04 15:17:14 +0000

    Update testoutput.go

--- testoutput.go
+++ testoutput.go
@@ -1,28 +1,20 @@
 package testoutput
 
 import (
-	"io"
-	"net/http"
-	. "github.com/mozilla-services/heka/pipeline"
-	"github.com/mozilla-services/heka/plugins"
-	"golang.org/x/net/websocket"
+        "io"
+        "net/http"
+        "golang.org/x/net/websocket"
 )
 
 func echoHandler(ws *websocket.Conn) {
-	io.Copy(ws, ws)
+        io.Copy(ws, ws)
 }
 
 func main() {
-	http.Handle("/echo", websocket.Handler(echoHandler))
-	http.Handle("/", http.FileServer(http.Dir(".")))
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic("ListenAndServe: " + err.Error())
-	}
-}
-
-func init() {
-	RegisterPlugin("TestOutput", func() interface{} {
-		return new(TestOutput)
-	})
+        http.Handle("/echo", websocket.Handler(echoHandler))
+        http.Handle("/", http.FileServer(http.Dir(".")))
+        err := http.ListenAndServe(":8080", nil)
+        if err != nil {
+                panic("ListenAndServe: " + err.Error())
+        }
 }
