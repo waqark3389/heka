1edec16c9976cbee54672df070fb9e7c1f4f1ee6
waqark3389 <waqark3389@gmail.com>
2016-01-05 10:51:56 +0000

    Update testoutput.go

--- testoutput.go
+++ testoutput.go
@@ -1,20 +1,104 @@
-package testoutput
+package heka_websockets
 
 import (
-        "io"
-        "net/http"
+        "fmt"
+        "github.com/mozilla-services/heka/message"
+        "github.com/mozilla-services/heka/pipeline"
         "golang.org/x/net/websocket"
+        "net/http"
 )
 
-func echoHandler(ws *websocket.Conn) {
-        io.Copy(ws, ws)
+type connection struct {
+        ws   *websocket.Conn
+        send chan *message.Message
 }
 
-func main() {
-        http.Handle("/echo", websocket.Handler(echoHandler))
-        http.Handle("/", http.FileServer(http.Dir(".")))
-        err := http.ListenAndServe(":8080", nil)
-        if err != nil {
-                panic("ListenAndServe: " + err.Error())
+type WebSocketsOutputConfig struct {
+        Address string `toml:"address"`
+}
+
+type WebSocketsOutput struct {
+        conf        *WebSocketsOutputConfig
+        connections map[*connection]struct{}
+        register    chan *connection
+        unregister  chan *connection
+        broadcast   chan *message.Message
+}
+
+func (wso *WebSocketsOutput) ConfigStruct() interface{} {
+        return &WebSocketsOutputConfig{":5000"}
+}
+
+func (wso *WebSocketsOutput) Init(config interface{}) error {
+        wso.conf = config.(*WebSocketsOutputConfig)
+        wso.connections = make(map[*connection]struct{})
+        wso.register = make(chan *connection)
+        wso.unregister = make(chan *connection)
+        wso.broadcast = make(chan *message.Message, 256)
+
+        // Connections handler
+        go func() {
+                var conn *connection
+                var m *message.Message
+                for {
+                        select {
+                        case conn = <-wso.register:
+                                wso.connections[conn] = struct{}{}
+                        case conn = <-wso.unregister:
+                                delete(wso.connections, conn)
+                                close(conn.send)
+                        case m = <-wso.broadcast:
+                                for conn = range wso.connections {
+                                        select {
+                                        case conn.send <- m:
+                                        default:
+                                                delete(wso.connections, conn)
+                                                close(conn.send)
+                                                go conn.ws.Close()
+                                        }
+                                }
+                        }
+                }
+        }()
+
+        // Websocket server and connection handler
+        http.Handle("/hekaout", websocket.Handler(func(ws *websocket.Conn) {
+                c := &connection{ws, make(chan *message.Message, 256)}
+
+                wso.register <- c
+
+                defer func() {
+                        wso.unregister <- c
+                }()
+
+                var err error
+                for m := range c.send {
+                        if err = websocket.JSON.Send(ws, m); err != nil {
+                                fmt.Println("Websocket:", err.Error())
+                                break
+                        }
+                }
+        }))
+
+        go func() {
+                if err := http.ListenAndServe(wso.conf.Address, nil); err != nil {
+                        fmt.Println("Http:", err.Error())
+                }
+        }()
+
+        return nil
+}
+
+func (wso *WebSocketsOutput) Run(or pipeline.OutputRunner, h pipeline.PluginHelper) error {
+        for pc := range or.InChan() {
+                wso.broadcast <- pc.Message
+                //pc.Recycle(pc)
         }
+        return nil
+}
+
+func init() {
+        pipeline.RegisterPlugin("WebSocketsOutput", func() interface{} {
+                return new(WebSocketsOutput)
+        })
 }
